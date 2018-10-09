0d85ef75f1fe811b9ccf6a8a398243eb886056f1
chrycko9491@gmail.com <chrycko9491@gmail.com>
2018-10-08 22:13:42 -0400

    adding the movability of the drawn matrix on the screen.

--- tetris.js
+++ tetris.js
@@ -1,17 +1,34 @@
 const canvas = document.getElementById('tetris');
 const context = canvas.getContext('2d')
 
+context.scale(20,20);
+
 context.fillStyle = '#000';
 context.fillRect(0,0,canvas.width, canvas.height)
 
 const matrix = [
-    [0,0,0]
-    [1,1,1]
-    [0,1,0]
-]
+    [0,0,0],
+    [1,1,1],
+    [0,1,0],
+];
 
 
+function drawMatrix(matrix, offset){
+    matrix.forEach((row,y) =>{
+        row.forEach((value, x)=>{
+            if(value !== 0){
+                context.fillStyle='red';
+                context.fillRect(x + offset.x,
+                                 y + offset.y,
+                                 1,1);
+            }
+        });
+    });
+};
 
+const player
+
+drawMatrix(matrix,{x: 5, y:5 });
 
 
 
