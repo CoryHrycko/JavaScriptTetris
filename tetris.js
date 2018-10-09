434cf8ebb782eb00221f62212a41c6f60cecfcd2
chrycko9491@gmail.com <chrycko9491@gmail.com>
2018-10-08 22:01:36 -0400

    Adding the first block the t block and drawing it on the canvas.

---
+++ tetris.js
@@ -0,0 +1,20 @@
+const canvas = document.getElementById('tetris');
+const context = canvas.getContext('2d')
+
+context.fillStyle = '#000';
+context.fillRect(0,0,canvas.width, canvas.height)
+
+const matrix = [
+    [0,0,0]
+    [1,1,1]
+    [0,1,0]
+]
+
+
+
+
+
+
+
+
+
