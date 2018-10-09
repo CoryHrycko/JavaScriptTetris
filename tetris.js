f2541e3e971766464f4b5b923ccaeabab5b08ad4
chrycko9491@gmail.com <chrycko9491@gmail.com>
2018-10-08 22:20:18 -0400

    writting in comments so it is easily maintainable.

--- tetris.js
+++ tetris.js
@@ -1,21 +1,26 @@
+//the play area
 const canvas = document.getElementById('tetris');
+// the play blocks
 const context = canvas.getContext('2d')
-
+//scalling the blocks
 context.scale(20,20);
-
+//applying the background
 context.fillStyle = '#000';
 context.fillRect(0,0,canvas.width, canvas.height)
-
+//first block matrix
 const matrix = [
     [0,0,0],
     [1,1,1],
     [0,1,0],
 ];
 
-
+//the drawing function
 function drawMatrix(matrix, offset){
+    //for each to replicate all the variables in each row.
     matrix.forEach((row,y) =>{
+       //allows the loop to go down to the next line. to draw the shape
         row.forEach((value, x)=>{
+            //the actual identifying of the 0 and 1 to make them red
             if(value !== 0){
                 context.fillStyle='red';
                 context.fillRect(x + offset.x,
@@ -28,6 +33,7 @@ function drawMatrix(matrix, offset){
 
 const player
 
+//draws the shape.
 drawMatrix(matrix,{x: 5, y:5 });
 
 
